69aabb142dc9e69811840eba448d82692204eee7
Pedro Haluch <pedro.haluch@gmail.com>
2023-05-14 09:40:51 -0300

    Merged scripts

---
+++ test/bmp280.cpp
@@ -0,0 +1,60 @@
+/* This code is to use with Adafruit BMP280           (Metric)
+ * It measures both temperature and pressure and it displays them on the Serial monitor with the altitude
+ * It's a modified version of the Adafruit example code
+ * Refer to www.surtrtech.com or SurtrTech Youtube channel
+ */
+
+#include <Adafruit_BMP280.h>
+
+Adafruit_BMP280 bmp; // I2C Interface
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println(F("BMP280 test"));
+
+  bool sensorFound = false;
+  for (uint8_t i = 0x00; i <= 0xFF; i++)
+  {
+    Serial.println(i);
+    if (bmp.begin(0x76))
+    {
+      sensorFound = true;
+      Serial.print(F("BMP280 sensor found at address: 0x"));
+      Serial.println(i, HEX);
+      break;
+    }
+  }
+
+  if (!sensorFound)
+  {
+    Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
+    while (1)
+      ;
+  }
+
+  /* Default settings from datasheet. */
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+}
+
+void loop()
+{
+  Serial.print(F("Temperature = "));
+  Serial.print(bmp.readTemperature());
+  Serial.println(" *C");
+
+  Serial.print(F("Pressure = "));
+  Serial.print(bmp.readPressure() / 100); // displaying the Pressure in hPa, you can change the unit
+  Serial.println(" hPa");
+
+  Serial.print(F("Approx altitude = "));
+  Serial.print(bmp.readAltitude(1013)); // The "1019.66" is the pressure(hPa) at sea level in day in your region
+  Serial.println(" m");                    // If you don't know it, modify it until you get your current altitude
+
+  Serial.println();
+  delay(2000);
+}
