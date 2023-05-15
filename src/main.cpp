ecea5ad0835e859bd79ade69dcea3e8087d3185e
Pedro Haluch <pedro.haluch@gmail.com>
2023-05-15 20:18:59 -0300

    Test code that should print GPS stuff if it connects.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,3 @@
-#include <TinyGPS++.h>
-#include <HardwareSerial.h>
 #include <Wire.h>
 #include <SPI.h>
 #include <SD.h>
@@ -11,18 +9,12 @@
 #include <TinyGPSPlus.h>
 #include <SoftwareSerial.h>
 /*
-   This sample code tracks satellite elevations using TinyGPSCustom objects.
-
-   Satellite numbers and elevations are not normally tracked by TinyGPSPlus, but 
-   by using TinyGPSCustom we get around this.
-
-   It requires the use of SoftwareSerial and assumes that you have a
-   4800-baud serial GPS device hooked up on pins 4(RX) and 3(TX).
+   This sample code demonstrates the normal use of a TinyGPSPlus (TinyGPSPlus) object.
+   It requires the use of SoftwareSerial, and assumes that you have a
+   4800-baud serial GPS device hooked up on pins 4(rx) and 3(tx).
 */
 static const int RXPin = 16, TXPin = 17;
-static const uint32_t GPSBaud = 4800;
-static const int MAX_SATELLITES = 40;
-static const int PAGE_LENGTH = 40;
+static const uint32_t GPSBaud = 9600;
 
 // The TinyGPSPlus object
 TinyGPSPlus gps;
@@ -30,134 +22,150 @@ TinyGPSPlus gps;
 // The serial connection to the GPS device
 SoftwareSerial ss(RXPin, TXPin);
 
-TinyGPSCustom totalGPGSVMessages(gps, "GPGSV", 1); // $GPGSV sentence, first element
-TinyGPSCustom messageNumber(gps, "GPGSV", 2);      // $GPGSV sentence, second element
-TinyGPSCustom satNumber[4]; // to be initialized later
-TinyGPSCustom elevation[4];
-bool anyChanges = false;
-unsigned long linecount = 0;
-
-struct
+// This custom version of delay() ensures that the gps object
+// is being "fed".
+static void smartDelay(unsigned long ms)
 {
-  int elevation;
-  bool active;
-} sats[MAX_SATELLITES];
-
-void IntPrint(int n, int len)
-{
-  int digs = n < 0 ? 2 : 1;
-  for (int i=10; i<=abs(n); i*=10)
-    ++digs;
-  while (digs++ < len)
-    Serial.print(F(" "));
-  Serial.print(n);
-  Serial.print(F(" "));
+  unsigned long start = millis();
+  do
+  {
+    while (ss.available())
+      gps.encode(ss.read());
+  } while (millis() - start < ms);
 }
 
-void TimePrint()
+static void printFloat(float val, bool valid, int len, int prec)
 {
-  if (gps.time.isValid())
+  if (!valid)
   {
-    if (gps.time.hour() < 10)
-      Serial.print(F("0"));
-    Serial.print(gps.time.hour());
-    Serial.print(F(":"));
-    if (gps.time.minute() < 10)
-      Serial.print(F("0"));
-    Serial.print(gps.time.minute());
-    Serial.print(F(":"));
-    if (gps.time.second() < 10)
-      Serial.print(F("0"));
-    Serial.print(gps.time.second());
-    Serial.print(F(" "));
+    while (len-- > 1)
+      Serial.print('*');
+    Serial.print(' ');
   }
   else
   {
-    Serial.print(F("(unknown)"));
+    Serial.print(val, prec);
+    int vi = abs((int)val);
+    int flen = prec + (val < 0.0 ? 2 : 1); // . and -
+    flen += vi >= 1000 ? 4 : vi >= 100 ? 3
+                         : vi >= 10    ? 2
+                                       : 1;
+    for (int i = flen; i < len; ++i)
+      Serial.print(' ');
   }
+  smartDelay(0);
 }
 
-void printHeader()
+static void printInt(unsigned long val, bool valid, int len)
 {
-  Serial.println();
-  Serial.print(F("Time     "));
-  for (int i=0; i<MAX_SATELLITES; ++i)
+  char sz[32] = "*****************";
+  if (valid)
+    sprintf(sz, "%ld", val);
+  sz[len] = 0;
+  for (int i = strlen(sz); i < len; ++i)
+    sz[i] = ' ';
+  if (len > 0)
+    sz[len - 1] = ' ';
+  Serial.print(sz);
+  smartDelay(0);
+}
+
+static void printDateTime(TinyGPSDate &d, TinyGPSTime &t)
+{
+  if (!d.isValid())
   {
-    Serial.print(F(" "));
-    IntPrint(i+1, 2);
+    Serial.print(F("********** "));
   }
-  Serial.println();
-  Serial.print(F("---------"));
-  for (int i=0; i<MAX_SATELLITES; ++i)
-    Serial.print(F("----"));
-  Serial.println();
+  else
+  {
+    char sz[32];
+    sprintf(sz, "%02d/%02d/%02d ", d.month(), d.day(), d.year());
+    Serial.print(sz);
+  }
+
+  if (!t.isValid())
+  {
+    Serial.print(F("******** "));
+  }
+  else
+  {
+    char sz[32];
+    sprintf(sz, "%02d:%02d:%02d ", t.hour(), t.minute(), t.second());
+    Serial.print(sz);
+  }
+
+  printInt(d.age(), d.isValid(), 5);
+  smartDelay(0);
+}
+
+static void printStr(const char *str, int len)
+{
+  int slen = strlen(str);
+  for (int i = 0; i < len; ++i)
+    Serial.print(i < slen ? str[i] : ' ');
+  smartDelay(0);
 }
 
 void setup()
 {
-  Serial.begin(115200);
+  Serial.begin(9600);
   ss.begin(GPSBaud);
 
-  Serial.println(F("SatElevTracker.ino"));
-  Serial.println(F("Displays GPS satellite elevations as they change"));
-  Serial.print(F("Testing TinyGPSPlus library v. ")); Serial.println(TinyGPSPlus::libraryVersion());
+  Serial.println(F("FullExample.ino"));
+  Serial.println(F("An extensive example of many interesting TinyGPSPlus features"));
+  Serial.print(F("Testing TinyGPSPlus library v. "));
+  Serial.println(TinyGPSPlus::libraryVersion());
   Serial.println(F("by Mikal Hart"));
   Serial.println();
-  
-  // Initialize all the uninitialized TinyGPSCustom objects
-  for (int i=0; i<4; ++i)
-  {
-    satNumber[i].begin(gps, "GPGSV", 4 + 4 * i); // offsets 4, 8, 12, 16
-    elevation[i].begin(gps, "GPGSV", 5 + 4 * i); // offsets 5, 9, 13, 17
-  }
-  Serial.println(F("Setup finished."));
+  Serial.println(F("Sats HDOP  Latitude   Longitude   Fix  Date       Time     Date Alt    Course Speed Card  Distance Course Card  Chars Sentences Checksum"));
+  Serial.println(F("           (deg)      (deg)       Age                      Age  (m)    --- from GPS ----  ---- to Brasilia  ----  RX    RX        Fail"));
+  Serial.println(F("----------------------------------------------------------------------------------------------------------------------------------------"));
 }
 
 void loop()
 {
-  // Dispatch incoming characters
-  if (ss.available() > 0)
-  {
-    Serial.println(F("ss.available() > 0"));
-    gps.encode(ss.read());
-    Serial.println(F("gps.encode(ss.read());"));
-    if (totalGPGSVMessages.isUpdated())
-    {
-      Serial.println(F(totalGPGSVMessages.isUpdated()));
-      for (int i=0; i<4; ++i)
-      {
-        int no = atoi(satNumber[i].value());
-        if (no >= 1 && no <= MAX_SATELLITES)
-        {
-          int elev = atoi(elevation[i].value());
-          sats[no-1].active = true;
-          if (sats[no-1].elevation != elev)
-          {
-            sats[no-1].elevation = elev;
-            anyChanges = true;
-          }
-        }
-      }
-      
-      int totalMessages = atoi(totalGPGSVMessages.value());
-      int currentMessage = atoi(messageNumber.value());
-      if (totalMessages == currentMessage && anyChanges)
-      {
-        if (linecount++ % PAGE_LENGTH == 0)
-          printHeader();
-        TimePrint();
-        for (int i=0; i<MAX_SATELLITES; ++i)
-        {
-          Serial.print(F(" "));
-          if (sats[i].active)
-            IntPrint(sats[i].elevation, 2);
-          else
-            Serial.print(F("   "));
-          sats[i].active = false;
-        }
-        Serial.println();
-        anyChanges = false;
-      }
-    }
-  }
+  static const double BRASILIA_LAT = -15.73905, BRASILIA_LON = -47.89370;
+
+  printInt(gps.satellites.value(), gps.satellites.isValid(), 5);
+  printFloat(gps.hdop.hdop(), gps.hdop.isValid(), 6, 1);
+  printFloat(gps.location.lat(), gps.location.isValid(), 11, 6);
+  printFloat(gps.location.lng(), gps.location.isValid(), 12, 6);
+  printInt(gps.location.age(), gps.location.isValid(), 5);
+  printDateTime(gps.date, gps.time);
+  printFloat(gps.altitude.meters(), gps.altitude.isValid(), 7, 2);
+  printFloat(gps.course.deg(), gps.course.isValid(), 7, 2);
+  printFloat(gps.speed.kmph(), gps.speed.isValid(), 6, 2);
+  printStr(gps.course.isValid() ? TinyGPSPlus::cardinal(gps.course.deg()) : "*** ", 6);
+
+  unsigned long distanceKmToBrasilia =
+      (unsigned long)TinyGPSPlus::distanceBetween(
+          gps.location.lat(),
+          gps.location.lng(),
+          BRASILIA_LAT,
+          BRASILIA_LON) /
+      1000;
+  printInt(distanceKmToBrasilia, gps.location.isValid(), 9);
+
+  double courseToBrasilia =
+      TinyGPSPlus::courseTo(
+          gps.location.lat(),
+          gps.location.lng(),
+          BRASILIA_LAT,
+          BRASILIA_LON);
+
+  printFloat(courseToBrasilia, gps.location.isValid(), 7, 2);
+
+  const char *cardinalToBrasilia = TinyGPSPlus::cardinal(courseToBrasilia);
+
+  printStr(gps.location.isValid() ? cardinalToBrasilia : "*** ", 6);
+
+  printInt(gps.charsProcessed(), true, 6);
+  printInt(gps.sentencesWithFix(), true, 10);
+  printInt(gps.failedChecksum(), true, 9);
+  Serial.println();
+
+  smartDelay(1000);
+
+  if (millis() > 5000 && gps.charsProcessed() < 10)
+    Serial.println(F("No GPS data received: check wiring"));
 }
